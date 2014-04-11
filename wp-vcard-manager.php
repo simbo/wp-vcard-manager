c581ee81d6594c87e9d29d0004d78ef04b0ca1ee
Simon Lepel <simbo@simbo.de>
2014-04-11 14:11:40 +0200

    version update

--- wp-vcard-manager.php
+++ wp-vcard-manager.php
@@ -3,7 +3,7 @@
   Plugin Name: wp-vcard-manager
   Plugin URI:  https://github.com/simbo/wp-vcard-manager
   Description: A WordPress plugin for managing and generating vcards, hcards and corresponding qrcode images.
-  Version:     0.0.2
+  Version:     0.0.3
   Author:      Simon Lepel
   Author URI:  http://simonlepel.de/
   License:     GPL
@@ -17,7 +17,7 @@ if( !class_exists('QRcode') )
     require_once 'vendor/qrcode/qrlib.php';
 
 require_once 'vendor/plugin-update-checker/plugin-update-checker.php';
-PucFactory::buildUpdateChecker('https://raw.githubusercontent.com/simbo/wp-vcard-manager/master/wp-plugin.json',__FILE__,'vcrdmngr');
+PucFactory::buildUpdateChecker('https://raw.githubusercontent.com/simbo/wp-vcard-manager/master/wp-plugin.json',__FILE__,'wp-vcard-manager');
 
 if( !class_exists('WPvCardManager') ) {
     require_once 'src/class-wp-vcard-manager.php';
